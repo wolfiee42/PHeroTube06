f77cc30c6105be865278103c9474e4ed7fbea007
wolfiee42 <saifalislam2022@gmail.com>
2023-08-31 22:03:22 +0600

    four main button added

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+/* fetch('https://openapi.programming-hero.com/api/videos/category/1000')
+    .then(res => res.json())
+    .then(data => datass(data));
+
+function datass(data){
+    const something = data.data;
+    something.forEach(datum => console.log(datum));
+} */
+
+/* fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    .then(res => res.json())
+    .then(data => datass(data));
+
+function datass(data){
+    const something = data.data;
+    something.forEach(datum => console.log(datum));
+} */
+
+
+const mainContainer = document.getElementById('main-section-container');
+mainContainer.innerHTML = `
+    <button class="px-3 rounded-md font-semibold py-1 mx-2 bg-slate-300">All</button>
+    <button class="px-3 rounded-md font-semibold py-1 mx-2 bg-slate-300">Music</button>
+    <button class="px-3 rounded-md font-semibold py-1 mx-2 bg-slate-300">Comedy</button>
+    <button class="px-3 rounded-md font-semibold py-1 mx-2 bg-slate-300">Drawing</button>
+`;
