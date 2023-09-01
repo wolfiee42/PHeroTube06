c5803e71b0210de55e45d6f596bf17e6fbbdfa4d
wolfiee42 <saifalislam2022@gmail.com>
2023-09-01 09:53:45 +0600

    fetch and data shown on webpage done

--- scripts/index.js
+++ scripts/index.js
@@ -14,35 +14,58 @@ fetch('https://openapi.programming-hero.com/api/videos/category/1000')
 
 function datass(data) {
     const something = data.data;
-    something.forEach(datum => {
-        all(datum);
-        // console.log(datum);
-    });
+    all(something);
 }
 
-const all = (datum) => {
-    const cardContainer = document.getElementById('card-container');
-    const divi = document.createElement('div');
-    // cardContainer.textContent = '';
-    divi.innerHTML = `
-    <div class="m-4">
-        <div>
-            <img class="w-[300px] h-[150px] mx-auto rounded-lg" src="${datum.thumbnail}">
-        </div>
-        <div class="flex justify-between items-center gap-4 mx-5 mt-5">
-            <div>
-                <img class="w-[50px] h-[50px] rounded-full" src="${datum.authors[0].profile_picture}">
-            </div>
-            <p class="font-bold">${datum.title}<p>
-        </div>
-        <div class="mx-10 my-2">
-            <p>${datum.authors[0].profile_name}</p>
-            <p>${datum.others.views} views</p>
-        </div>
-    </div>
-`;
-    cardContainer.appendChild(divi);
+function infoo(id) {
+    fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            const dataass = data.data;
+            all(dataass)
+        })
+}
 
+
+document.getElementById('all-btn').addEventListener('click', () => {
+    infoo(1000)
+})
+document.getElementById('music-btn').addEventListener('click', () => {
+    infoo(1001)
+})
+document.getElementById('comedy-btn').addEventListener('click', () => {
+    infoo(1003)
+})
+document.getElementById('drawing-btn').addEventListener('click', () => {
+    infoo(1005)
+})
+
+const all = (data) => {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.textContent = '';
+
+    data.forEach(datum => {
+        const divi = document.createElement('div');
+
+        divi.innerHTML = `
+            <div class="m-4">
+                <div>
+                    <img class="w-[300px] h-[150px] mx-auto rounded-lg" src="${datum.thumbnail}">
+                </div>
+                <div class="flex justify-between items-center gap-4 mx-5 mt-5">
+                    <div>
+                        <img class="w-[50px] h-[50px] rounded-full" src="${datum.authors[0].profile_picture}">
+                    </div>
+                    <p class="font-bold">${datum.title}<p>
+                </div>
+                <div class="mx-10 my-2">
+                    <p>${datum.authors[0].profile_name}</p>
+                    <p>${datum.others.views} views</p>
+                </div>
+            </div>`;
+
+        cardContainer.appendChild(divi);
+    })
 }
 
 
@@ -50,26 +73,3 @@ const all = (datum) => {
 
 
 
-function infoo(id) {
-    fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
-        .then(res => res.json())
-        .then(data => {
-            const dataass = data.data;
-
-            dataass.forEach(datum => {
-                console.log(datum);
-                all(datum)
-            })
-        })
-}
-
-
-document.getElementById('all-btn').addEventListener('click', (id) => {
-    infoo(1000)
-})
-document.getElementById('music-btn').addEventListener('click', (id) => {
-    infoo(1001)
-})
-document.getElementById('comedy-btn').addEventListener('click', (id) => {
-    infoo(1003)
-})
