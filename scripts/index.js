50aa1c1995a0c2d62856f18b346040212aedd7ac
wolfiee42 <saifalislam2022@gmail.com>
2023-09-03 16:02:24 +0600

    reassmbled

--- scripts/index.js
+++ scripts/index.js
@@ -1,90 +1,63 @@
-const mainContainer = document.getElementById('main-section-container');
-mainContainer.innerHTML = `
-    <button id="all-btn" class="px-3 rounded-md font-semibold py-1 mx-2 bg-slate-300 hover:bg-slate-400">All</button>
-    <button id="music-btn" class="px-3 rounded-md font-semibold py-1 mx-2 bg-slate-300 hover:bg-slate-400">Music</button>
-    <button id="comedy-btn" class="px-3 rounded-md font-semibold py-1 mx-2 bg-slate-300 hover:bg-slate-400">Comedy</button>
-    <button id="drawing-btn" class="px-3 rounded-md font-semibold py-1 mx-2 bg-slate-300 hover:bg-slate-400">Drawing</button>
-`;
+const categories = async () => {
 
-// default section
-fetch('https://openapi.programming-hero.com/api/videos/category/1000')
-    .then(res => res.json())
-    .then(data => datass(data));
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await response.json();
+    const items = data.data;
+
+    // btn section 
+    const tabSection = document.getElementById('tab-container')
 
 
+    //dynamically creating btn section
 
-function datass(data) {
-    const something = data.data;
-    all(something);
+    // if i want to select specific 
+    // items.slice(0, 2).forEach
 
+    // else normally
+    items.forEach((datum) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a onclick="handleCategoryId('${datum.category_id}')" class="btn btn-error hover:bg-white ">${datum.category}</a>`;
+        tabSection.appendChild(div)
+    })
 }
 
-// for all section
-function infoo(id) {
-    fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
-        .then(res => res.json())
-        .then(data => {
-            const dataass = data.data;
-            all(dataass);
-            console.log(dataass);
-            console.log(data);
 
-            if (dataass.length > 0) {
-                all(dataass);
-            } else if (dataass.length === 0) {
-                nothing();
+
+
+
+// with this function im linking category name and categoryId and calling this function from btn onclick
+const handleCategoryId = async (categoryId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+    const data = await response.json();
+    const situation = data.status
+    const datumm = data.data;
+
+    if (situation) {
+        const cardcontainer = document.getElementById('card-container');
+        cardcontainer.textContent = '';
+        cardcontainer.classList = `grid grid-rows-1 md:grid-cols-2 lg:grid-cols-4 gap-4`
+        datumm.forEach(datum => {
+
+            function secToHM(totalSeconds) {
+                const secInMin = 60;
+                const secInHr = 60 * secInMin;
+    
+    
+                const hours = Math.floor(totalSeconds / secInHr);
+                const minutes = Math.floor((totalSeconds % secInHr) / secInMin);
+    
+                return { hours, minutes };
             }
-        })
-
-}
-
-
-
-// indivitual calling
-document.getElementById('all-btn').addEventListener('click', () => {
-    infoo(1000);
-})
-document.getElementById('music-btn').addEventListener('click', () => {
-    infoo(1001)
-})
-document.getElementById('comedy-btn').addEventListener('click', () => {
-    infoo(1003)
-})
-document.getElementById('drawing-btn').addEventListener('click', () => {
-    infoo(1005)
-})
-
-const all = (data) => {
-    const cardContainer = document.getElementById('card-container');
-    cardContainer.textContent = '';
-
-    data.forEach(datum => {
-
-
-        function secToHM(totalSeconds) {
-            const secInMin = 60;
-            const secInHr = 60 * secInMin;
-
-
-            const hours = Math.floor(totalSeconds / secInHr);
-            const minutes = Math.floor((totalSeconds % secInHr) / secInMin);
-
-            return { hours, minutes };
-        }
-
-        const totalSeconds = datum.others.posted_date;
-        const { hours, minutes } = secToHM(totalSeconds);
-
-
-
-        const divi = document.createElement('div');
-
-        divi.innerHTML = `
-            <div class="m-4">
+    
+            const totalSeconds = datum.others.posted_date;
+            const { hours, minutes } = secToHM(totalSeconds);
+            const div = document.createElement('div');
+            div.innerHTML = `
+        <div class="m-4">
                 <div class="relative">
                     <img class="w-[300px] h-[150px] mx-auto rounded-lg" src="${datum.thumbnail}">
-                    
-                        <p>${datum.others.posted_date ? `<div class="absolute bottom-2 right-10 md:right-8 lg:right-5 bg-slate-100 p-1 text-black rounded-lg text-xs">${hours}hrs ${minutes}mins ago</div>` : ' '}</p>
+                    <p>${datum.others.posted_date ? `<div class="absolute bottom-2 right-10 md:right-8 lg:right-5 bg-slate-100 p-1 text-black rounded-lg text-xs">${hours}hrs ${minutes}mins ago</div>` : ' '}</p>
                     
                 </div>
                 <div class="flex justify-center lg:justify-between items-center gap-8 lg:gap-4 mx-5 mt-5">
@@ -100,28 +73,31 @@ const all = (data) => {
                     </div>
                     <p>${datum.others.views} views</p>
                 </div>
-            </div>`;
+            </div>
+        `
+            cardcontainer.appendChild(div)
+        })
+    } else {
+        const cardContainer = document.getElementById('card-container');
+        cardContainer.classList = ``
+        cardContainer.textContent = '';
 
-        cardContainer.appendChild(divi);
-    })
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <img src="image/Icon.png">
+            <p>Oops!! Sorry, There is no content here.</p>
+            `;
+        div.classList = `flex flex-col justify-center items-center gap-5 my-28`
+        cardContainer.appendChild(div);
+    }
+
+
+    console.log(situation);
+    console.log(datumm);
+
 }
-
-const nothing = () => {
-    const cardContainer = document.getElementById('card-containerr');
-    cardContainer.textContent = '';
-
-    const div = document.createElement('div');
-    div.innerHTML = `
-    <img src="image/Icon.png">
-    <p>Oops!! Sorry, There is no content here.</p>
-    `;
-    div.classList = `flex flex-col justify-center items-center gap-5 my-28`
-    cardContainer.appendChild(div);
-}
-
-document.getElementById('sort-btn').addEventListener('click', ()=>{
-    location.reload();
-})
+categories()
+handleCategoryId(1000);
 
 document.getElementById('blogBtn').addEventListener('click', ()=> {
     window.location.href= 'blog.html';
